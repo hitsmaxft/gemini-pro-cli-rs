0fa4060f22bfb4ec57dd08b9bd92e065dac59146
hitsmaxft <mfthits@gmail.com>
2024-02-04 20:06:38 +0800

    fmt

--- flake.nix
+++ flake.nix
@@ -7,40 +7,28 @@
 
   outputs = { self, nixpkgs, utils, naersk }:
   let
-    pversion = "v0.5.14";
-    systemBuildInputs = pkgs: {
-      x86_64-linux = [
-        pkgs.iconv
-      ];
-      x86_64-darwin =  [
-        pkgs.iconv
-        pkgs.darwin.apple_sdk.frameworks.Security
-        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-      ];
-    };
-
+    systemBuildInputs = system: pkgs: {
+      ${system} = [ pkgs.iconv pkgs.openssl ]  ++ 
+      (if pkgs.lib.strings.hasInfix "$system" "darwin"
+      then [ pkgs.darwin.apple_sdk.frameworks.Security pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ]
+      else [ ]);
+      };
   in
-  utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" ] (system:
+  utils.lib.eachDefaultSystem( system:
   let
     pkgs = import nixpkgs { inherit system; };
     naersk-lib = pkgs.callPackage naersk { };
-
-    src  =  pkgs.fetchzip {
-      name = "src";
-      url = "https://github.com/zurawiki/gptcommit/archive/refs/tags/${pversion}.tar.gz";
-      hash = "sha256-xjaFr1y2Fd7IWbJlegnIsfS5/oMJYd6QTnwp7IK17xM=";
-    };
   in
   {
     defaultPackage = naersk-lib.buildPackage  {
-      inherit src;
-      buildInputs = (systemBuildInputs pkgs).${system};
+      src = ./.;
+      buildInputs = (systemBuildInputs system pkgs).${system};
     };
 
     devShell = with pkgs; mkShell {
       buildInputs = [ 
         cargo rustc rustfmt pre-commit rustPackages.clippy 
-      ] ++ (systemBuildInputs pkgs).${system};
+      ] ++ (systemBuildInputs system pkgs).${system};
       RUST_SRC_PATH = rustPlatform.rustLibSrc;
     };
   });
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate shellexpand; // 1.0.0
 
-
-use log::info;
 use clap::{App, Arg, ArgMatches};
 use env_logger::Env;
 use google_generative_ai_rs::v1::gemini::response::{Candidate, GeminiResponse};
-use std::io::{stdout, Write};
+use log::info;
 use serde::{Deserialize, Serialize};
+use std::io::{stdout, Write};
 
 use google_generative_ai_rs::v1::{
     api::Client,
@@ -20,7 +19,6 @@ struct Config {
 }
 
 async fn read_config(input: &str) -> Result<Config, Box<dyn std::error::Error>> {
-
     let real_path: &str = &shellexpand::tilde(input);
 
     info!("final config file path is {}", real_path);
@@ -30,24 +28,23 @@ async fn read_config(input: &str) -> Result<Config, Box<dyn std::error::Error>> 
 }
 
 async fn output_response(gemini: &GeminiResponse) -> String {
-
-    if gemini.candidates.len() ==0 {
+    if gemini.candidates.len() == 0 {
         return "".to_string();
     }
 
-    let first_candi:&Candidate = &gemini.candidates[0];
+    let first_candi: &Candidate = &gemini.candidates[0];
 
     if first_candi.content.parts.len() == 0 {
         return "".to_string();
     }
 
-    let first_part : &Part = &first_candi.content.parts[0];
+    let first_part: &Part = &first_candi.content.parts[0];
     let may_text: &Option<String> = &first_part.text;
 
-    match may_text  {
-        Some(text ) => {
+    match may_text {
+        Some(text) => {
             let mut lock = stdout().lock();
-            let _  = write!(lock, "{}", text);
+            let _ = write!(lock, "{}", text);
             "".to_string()
         }
         _ => "".to_string(),
@@ -55,16 +52,17 @@ async fn output_response(gemini: &GeminiResponse) -> String {
 }
 
 async fn run(matches: ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
-
-
     let env = Env::default()
-        .filter_or("MY_LOG_LEVEL", match matches.contains_id("verbose") {
-           true => "info",
-           _  => "warn", })
+        .filter_or(
+            "MY_LOG_LEVEL",
+            match matches.contains_id("verbose") {
+                true => "info",
+                _ => "warn",
+            },
+        )
         .write_style_or("MY_LOG_STYLE", "always");
 
     env_logger::init_from_env(env);
-
 
     // Parse command-line arguments
     let prompt = matches.value_of("prompt").unwrap_or_else(|| {
@@ -119,14 +117,19 @@ async fn run(matches: ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
         info!("streaming output");
         if let Some(stream_response) = response.streamed() {
             if let Some(json_stream) = stream_response.response_stream {
-                Client::for_each_async(json_stream, move |gr:GeminiResponse| async move {
+                Client::for_each_async(json_stream, move |gr: GeminiResponse| async move {
                     output_response(&gr).await;
-                }).await
+                })
+                .await
             }
         }
     } else {
         if let Some(gemini) = response.rest() {
-            if let Some(text) = &gemini.candidates.get(0).and_then(|c| c.content.parts.get(0).and_then(|p| p.text.as_ref())) {
+            if let Some(text) = &gemini
+                .candidates
+                .get(0)
+                .and_then(|c| c.content.parts.get(0).and_then(|p| p.text.as_ref()))
+            {
                 print!("{}", text);
             }
         }
@@ -145,36 +148,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .about("Interacts with the Gemini model")
         .arg(
             Arg::with_name("prompt")
-            .long("prompt")
-            .value_name("PROMPT")
-            .help("Sets the prompt for the Gemini model")
-            .takes_value(true),
+                .long("prompt")
+                .value_name("PROMPT")
+                .help("Sets the prompt for the Gemini model")
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("verbose")
-            .short('v')
-            .long("verbose")
-            .help("output more logs"),
+                .short('v')
+                .long("verbose")
+                .help("output more logs"),
         )
         .arg(
             Arg::with_name("stream")
-            .long("stream")
-            .help("Streams the response from the model"),
+                .long("stream")
+                .help("Streams the response from the model"),
         )
         .arg(
             Arg::with_name("config-file")
-            .short('f')
-            .long("config-file")
-            .value_name("FILE")
-            .help("Specify a custom TOML file for configuration")
-            .takes_value(true),
+                .short('f')
+                .long("config-file")
+                .value_name("FILE")
+                .help("Specify a custom TOML file for configuration")
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("token")
-            .long("token")
-            .value_name("TOKEN")
-            .help("Specify the API token directly")
-            .takes_value(true),
+                .long("token")
+                .value_name("TOKEN")
+                .help("Specify the API token directly")
+                .takes_value(true),
         )
         .get_matches();
 
